1c11ab6922cfaa9f3207f1c161d8e1c8fb551530
Alaina Traxler <eloavox@gmail.com>
2016-12-05 12:41:38 -0800

    add Post model.

--- app/src/main/java/com/example/guest/barbelith/models/post.java
+++ app/src/main/java/com/example/guest/barbelith/models/post.java
@@ -3,10 +3,22 @@ package com.example.guest.barbelith.models;
 /**
  * Created by Guest on 12/5/16.
  */
-public class post {
+public class Post {
     String title;
     String content;
     String author;
-    int upvotes;
-    int downvotes;
+    int upvotes = 0;
+    int downvotes = 0;
+
+    public Post(String _title, String _content, String _author){
+        title = _title;
+        content = _content;
+        author = _author;
+    }
+
+    public String getTitle(){ return title; }
+    public String getContent(){ return content; }
+    public String getAuthor(){ return author; }
+    public int getUpvotes(){ return upvotes; }
+    public int getDownvotes(){ return downvotes; }
 }
