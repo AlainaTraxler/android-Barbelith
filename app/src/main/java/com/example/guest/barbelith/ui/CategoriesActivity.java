bdf49c5918da775794551e458f1d91be7da1a2ca
Alaina Traxler <eloavox@gmail.com>
2016-12-05 09:43:29 -0800

    migrate ui Activities into ui package.

--- app/src/main/java/com/example/guest/barbelith/ui/CategoriesActivity.java
+++ app/src/main/java/com/example/guest/barbelith/ui/CategoriesActivity.java
@@ -1,13 +1,15 @@
-package com.example.guest.barbelith;
+package com.example.guest.barbelith.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.guest.barbelith.R;
+
+public class CategoriesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_categories);
     }
 }
