4488e016fe9ca3f41a5c001bf5b18814f28ae2a9
Alaina Traxler <eloavox@gmail.com>
2016-12-05 13:54:17 -0800

    create and link NewPostActivity

--- app/src/main/java/com/example/guest/barbelith/ui/CategoryActivity.java
+++ app/src/main/java/com/example/guest/barbelith/ui/CategoryActivity.java
@@ -3,18 +3,25 @@ package com.example.guest.barbelith.ui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.guest.barbelith.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.imageView_NewPost) ImageView mImageView_NewPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,21 @@ public class CategoryActivity extends AppCompatActivity {
 
         ActionBar actionBar;
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_Category);
+        setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
-        ColorDrawable colorDrawable = new ColorDrawable(getResources().getColor(R.color.colorConversationMain));
         actionBar.setBackgroundDrawable(new ColorDrawable(intent.getIntExtra("mainColor", 0)));
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         ref.child("Test").setValue("Success");
+
+        mImageView_NewPost.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        if(v == mImageView_NewPost){
+            Intent intent = new Intent(CategoryActivity.this, NewPostActivity.class);
+            startActivity(intent);
+        }
     }
 }
